a2b7ba28fdc81b6364fe553ad0f2897a9ac91d77
Jaime Perez <clavejin18@gmail.com>
2019-04-23 01:33:27 -0700

    updates

---
+++ assets/javascript/mySite.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  // var config = {
+  //   apiKey: "AIzaSyCs8x8AIQc2PvrmNSZP068_0UhrdzV6Mj4",
+  //   authDomain: "my-personal-website-2f76b.firebaseapp.com",
+  //   databaseURL: "https://my-personal-website-2f76b.firebaseio.com",
+  //   projectId: "my-personal-website-2f76b",
+  //   storageBucket: "my-personal-website-2f76b.appspot.com",
+  //   messagingSenderId: "292917542306"
+  // };
+  // firebase.initializeApp(config);
